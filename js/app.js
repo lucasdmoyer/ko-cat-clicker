a5908a81173482c7e6f64b85d66115736b3b40b5
Lucas Moyer <Lucas Moyer>
2017-01-14 22:05:32 -0800

    type: feat.
    body: Added more cats and way to select more cats
    
    footer: Version 3

--- js/app.js
+++ js/app.js
@@ -1,20 +1,75 @@
-var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
-	this.incrementcounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-	};
-}
+var initialCats =  [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'images/cat1.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        	nicknames: ['asdf', 'asdfdsa']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'images/cat1.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+        	nicknames: ['asdf', 'asdfdsa', 'asdfas']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'images/cat1.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'images/cat1.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'images/cat1.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+        }
+    ]
 
-var Cat = function() {
-	this.clickCount = ko.observable(0);
+// cat list
+// make cats clickable
+
+var ViewModel = function() {
+	var self = this;
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+
+
+	this.currentCat = ko.observable(this.catList()[0]);
+	// with the with: currentCat binding context, this is a cat
+	this.incrementcounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+
+	this.changeCat = function(cat) {
+		self.currentCat(cat);
+	};
+
+}
+/*
+var List = function() {
+	this.newList = ko.observableArray(ViewModel.catList);
+	console.log(this.newList);
+	return this.newList;
+} */
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
 	// connects to data-bind="text : name"
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/catpic1.jpg');
-	this.imgAttribution = ko.observable('httpsdfasdasdf');
-	this.nicknames = ko.observableArray([
-		{name: 'Bert'},
-		{name: 'Charles'}
-	]);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 5) {
@@ -23,6 +78,9 @@ var Cat = function() {
 			return 'adult';
 		}
 	}, this);
+
+
+
 }
 
 ko.applyBindings(new ViewModel());
