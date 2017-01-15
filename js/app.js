6258645bf25234f43c9ec2b796cef615c385fddc
Lucas Moyer <Lucas Moyer>
2017-01-14 20:16:46 -0800

    type: feat.
    body: Added nicknames
    
    footer: Version 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 var ViewModel = function() {
+	this.currentCat = ko.observable(new Cat());
+	this.incrementcounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+}
+
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	// connects to data-bind="text : name"
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/catpic1.jpg');
 	this.imgAttribution = ko.observable('httpsdfasdasdf');
+	this.nicknames = ko.observableArray([
+		{name: 'Bert'},
+		{name: 'Charles'}
+	]);
 
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 5) {
@@ -12,10 +23,6 @@ var ViewModel = function() {
 			return 'adult';
 		}
 	}, this);
-
-	this.incrementcounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
 }
 
 ko.applyBindings(new ViewModel());
